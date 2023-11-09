60790da2fa2a940de4b86fbbf08b476000aab411
izzaz <ahmad.mohamed.iqbal@accenture.com>
2023-11-10 03:37:23 +0800

    add : add additional files

--- src/main/resources/db/migration/V1_0__CREATE_TABLE.sql
+++ src/main/resources/db/migration/V1_0__CREATE_TABLE.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS postcodelatlng (
     id int NOT NULL AUTO_INCREMENT,
     postcode varchar(8) NOT NULL,
-    latitude decimal(10,7) NOT NULL,
-    longitude decimal(10,7) NOT NULL,
+    latitude decimal(10,7)  NULL,
+    longitude decimal(10,7)  NULL,
     PRIMARY KEY (id)
     );
